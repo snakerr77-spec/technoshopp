a4016f8ff53d56c7bdfa6d31cd1dff4467e49a09
DEV S?? <snakerr77@gmail.com>
2026-07-24 09:36:09 -0300

    Format vite.config.ts for better readability

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173, host: true },
-  build: { sourcemap: false }
+  base: "./",
+  server: {
+    port: 5173,
+    host: true,
+  },
+  build: {
+    sourcemap: false,
+  },
 });
