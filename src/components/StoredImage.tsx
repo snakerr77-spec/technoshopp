36a9c372e4d35577b89053e4f2702e69bbbac766
DEV S?? <snakerr77@gmail.com>
2026-07-24 12:04:02 -0300

    Delete src directory

--- src/components/StoredImage.tsx
+++
@@ -1,7 +0,0 @@
-import type { ImgHTMLAttributes } from "react";
-import { useImageSource } from "../lib/imageStore";
-
-export function StoredImage({ src, ...props }: ImgHTMLAttributes<HTMLImageElement>) {
-  const source = useImageSource(typeof src === "string" ? src : undefined);
-  return <img src={source} {...props} />;
-}
